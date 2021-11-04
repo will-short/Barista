98abec4b8ca976dd07e39142ea9fdef68f801270
will-short <willofmetal85@gmail.com>
2021-11-04 12:27:32 -0500

    login form css added.  still needs styled

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -32,24 +32,20 @@ function LoginFormPage() {
           <li key={idx}>{error}</li>
         ))}
       </ul>
-      <label>
-        Username or Email
-        <input
-          type="text"
-          value={credential}
-          onChange={(e) => setCredential(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
+      <input
+        type="text"
+        value={credential}
+        placeholder="username"
+        onChange={(e) => setCredential(e.target.value)}
+        required
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
       <button type="submit">Log In</button>
     </form>
   );
