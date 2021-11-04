5f838ef84ee46bf0a84b5ed5ec459c09cd7f62b0
will-short <willofmetal85@gmail.com>
2021-11-04 12:30:11 -0500

    added restore user thunk and added it to app.js

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -3,16 +3,20 @@ import { csrfFetch } from "./csrf";
 const SETUSER = "session/SETUSER";
 const REMOVEUSER = "session/removeUser";
 
+//set user action
 const setUser = (user) => ({
   type: SETUSER,
   user,
 });
+
+//remove user action
 const removeUser = () => {
   return {
     type: REMOVEUSER,
   };
 };
 
+//thunk for login
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch("/api/session", {
@@ -22,6 +26,14 @@ export const login = (user) => async (dispatch) => {
       password,
     }),
   });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+//thunk to restore user
+export const restoreUser = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session");
   const data = await response.json();
   dispatch(setUser(data.user));
   return response;
