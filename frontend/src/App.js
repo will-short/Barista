3f49af2404c85ba927665d82cfddaf596d772458
will-short <willofmetal85@gmail.com>
2021-11-04 12:00:57 -0500

    added react form for login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+
 function App() {
   return (
-    <>
-      <div>App Component</div>
-    </>
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
   );
 }
 
