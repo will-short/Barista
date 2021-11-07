7d6a88ec5e3c5ad6a10e66924b5f9d709bc3a869
will-short <willofmetal85@gmail.com>
2021-11-07 01:25:52 -0600

    added demo user

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,15 +1,17 @@
 import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
+import * as sessionActions from "../../store/session";
+
 import "./Navigation.css";
 import logo from "../../images/Barista-logo-text.png";
 
 function Navigation({ isLoaded }) {
+  const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
-
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = <ProfileButton user={sessionUser} />;
@@ -18,6 +20,19 @@ function Navigation({ isLoaded }) {
       <div className="userAuth">
         <LoginFormModal />
         <SignupFormModal />
+        <button
+          className="user"
+          onClick={() =>
+            dispatch(
+              sessionActions.login({
+                credential: "Demo-lition",
+                password: "password",
+              })
+            )
+          }
+        >
+          Demo User
+        </button>
       </div>
     );
   }
