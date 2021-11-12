6a99753beace63cfa129bf05032048daae3f87a3
will-short <willofmetal85@gmail.com>
2021-11-12 09:41:47 -0600

    fixes

--- frontend/src/components/Locations/index.js
+++ frontend/src/components/Locations/index.js
@@ -1,29 +1,46 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CheckinFeed from "../CheckinFeed";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import "./Locations.css";
+import GoogleMapReact from "google-map-react";
+
+// function MyMapComponent({ center, zoom }) {
+//   const [place, setPlace] = useState();
+//   const ref = useRef();
+//   let map;
+//   useEffect(() => {
+//     let map = new window.google.maps.Map(ref.current, {
+//       center,
+//       zoom,
+//     });
+//     const marker1 = {
+//       position: { lat: 40.51076, lng: -88.99346 },
+//       map,
+//     };
+//     new window.google.maps.Marker(marker1);
+//   });
+
+//   return (
+//     <div id="mapWrapper">
+//       <div ref={ref} id="map" />
+//     </div>
+//   );
+// }
 
 export default function Locations() {
-  const sessionUser = useSelector((state) => state.session.user);
-  let sideBar;
-  if (sessionUser) {
-  } else {
-    sideBar = (
-      <article>
-        <h2>Welcome to Barista, an Untappd clone.</h2>
-        <h3>
-          If you want to explore without creating a account click on the Demo
-          User button!
-        </h3>
-      </article>
-    );
-  }
+  const center = { lat: 40.4842, lng: -88.99369 };
+  const zoom = 12;
   return (
-    <main>
-      <div className="checkin-feed">
-        <h2>Recent Activity</h2>
-        <CheckinFeed />
+    <div id="locations">
+      <div id="mapWrapper">
+        <GoogleMapReact
+          apiKey={"AIzaSyBynTKh6jKkL6pn5gHvhOIgFjHUXLvVfAA"}
+          bootstrapURLKeys={{ key: "AIzaSyBynTKh6jKkL6pn5gHvhOIgFjHUXLvVfAA" }}
+          defaultCenter={center}
+          defaultZoom={zoom}
+        ></GoogleMapReact>
       </div>
-      <aside>{sideBar}</aside>
-    </main>
+    </div>
   );
 }
